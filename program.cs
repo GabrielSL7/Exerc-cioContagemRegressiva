57985e5ff60ba7fd4d177db211080a39d07b563b
GabrielSL7 <g21gabriel77@gmail.com>
2025-05-24 17:45:15 -0300

    program.cs

---
+++ program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Preparar...");
+        Console.ReadKey();
+        Console.WriteLine("Apontar...");
+        Console.ReadKey();
+        Console.WriteLine("FOGO!");
+        Console.ReadKey();
+    }
+}
